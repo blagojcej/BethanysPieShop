bbeb249d658810b6cada53ea78094a6e8ecda53c
blagojcej <blagojcejovanoski@yahoo.com>
2018-10-25 19:37:27 +0200

    BethanysPieShop Finished Project

---
+++ src/BethanysPieShop/Models/AppDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BethanysPieShop.Models
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=BethanysPieShop;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
